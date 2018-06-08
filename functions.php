fb6cd398f673afadd3f2f623c0ef21f4d166bdf0
Aleš Trunda <alestrunda@gmail.com>
2018-06-08 10:45:53 +0200

    add sass elements.code

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ function theme_styles() {
 	// Load our main stylesheet
 	wp_enqueue_style( 'slick-carousel', get_template_directory_uri() . '/slick-carousel/slick.css');
 	wp_enqueue_style( 'lightbox', get_template_directory_uri() . '/lightbox/css/lightbox.css');
-	wp_enqueue_style( 'main-style', get_stylesheet_uri());
+	wp_enqueue_style( 'main-style', get_stylesheet_uri(), array(), '1.0.1');
 }
 add_action( 'wp_enqueue_scripts', 'theme_styles' );
 
