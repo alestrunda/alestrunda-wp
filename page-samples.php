df5c6526ade292ad1a2e36269b9ae6e02a21c861
Aleš Trunda <alestrunda@gmail.com>
2018-12-07 09:22:49 +0100

    add page-samples

---
+++ page-samples.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Page samples
+ *
+ */
+
+	get_header();
+	?>
+    
+	<section class="section-content">
+    <div class="container">
+      <div class="grid grid--center">
+        <div class="grid__item grid__item--lg-span-4">
+          <div class="box-service">
+            <div class="box-service__head">
+              <div class="icon-opening icon-opening--cubes">Statický web</div>
+            </div>
+            <div class="price">
+              <span class="price__info">od</span> 7000<span class="price__info text-uppercase">kč</span>
+            </div>
+            <div class="box-service__content">
+              <div class="box-service__content-line">webové prezentace se statickým obsahem</div>
+              <div class="box-service__content-line">HTML, CSS, případně SASS, ITCSS...</div>
+              <div class="box-service__content-line">dynamické prvky - nastavení pluginů a vlastní funkcionalita</div>
+              <div class="box-service__content-line">nezahrnuje grafický návrh</div>
+              <div class="box-service__content-line">plně responzivní</div>
+              <div class="box-service__content-line">standardní on-page SEO</div>
+            </div>
+          </div><!-- .box-service -->
+        </div>
+      </div>
+    </div><!-- .container -->
+  </section>
+    
+  <?php
+	get_footer();
+?>
